c3fa014a7f0c75814fdeb9a133ff459f60080379
xKonuC <ignacio.lopez.1907@gmail.com>
2025-07-05 17:50:01 -0400

    este commit elimina todo codigo de saldo (inutilizable)

--- backend/dist/saldo/saldo/saldo.controller.d.ts
+++ backend/dist/saldo/saldo/saldo.controller.d.ts
@@ -2,7 +2,5 @@ import { SaldoService } from './saldo.service';
 export declare class SaldoController {
     private readonly saldoService;
     constructor(saldoService: SaldoService);
-    getSaldoByUserId(userId: number): Promise<import("./saldo.entity").Saldo | {
-        message: string;
-    }>;
+    getSaldoByUserId(userId: number): Promise<any>;
 }
--- backend/dist/saldo/saldo/saldo.controller.js
+++ backend/dist/saldo/saldo/saldo.controller.js
@@ -11,6 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SaldoController = void 0;
 const common_1 = require("@nestjs/common");
@@ -38,6 +39,6 @@ __decorate([
 ], SaldoController.prototype, "getSaldoByUserId", null);
 exports.SaldoController = SaldoController = __decorate([
     (0, common_1.Controller)('saldos'),
-    __metadata("design:paramtypes", [saldo_service_1.SaldoService])
+    __metadata("design:paramtypes", [typeof (_a = typeof saldo_service_1.SaldoService !== "undefined" && saldo_service_1.SaldoService) === "function" ? _a : Object])
 ], SaldoController);
 //# sourceMappingURL=saldo.controller.js.map
